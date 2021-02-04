58dd829f6963ccca0fa6fe272621fe9e856781bd
sb <stephane.baudry.vanaa@gmail.com>
2021-02-04 18:10:00 +0100

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Menu from "./components/Menu"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="appName">
+        <h1>Sorting Visualizer</h1>
+      </div>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
