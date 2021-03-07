aa1d7ef12cc2a8f290c53c214d4942e5cdba8efc
sb <stephane.baudry.vanaa@gmail.com>
2021-03-07 11:14:45 +0100

    changed explanations

--- src/components/Explanations.js
+++ src/components/Explanations.js
@@ -2,9 +2,9 @@ export default function Explanations(props) {
    
 return(
     <div>
-    <pre>1. Use the 1st slider on the far left to generate between 1 and 300 bars</pre>
-    <pre>2. (Optional) Use the 2st slider to alter the sorting speed (MAX = 1s)</pre>
-    <pre>3. Choose a sorting algorithm and enjoy the ride!</pre>
-    <pre>4. Repeat from step 1. and have fun 🙂</pre>
+    <pre>1.Use the 1st slider on the far left to generate bars</pre>
+    <pre>2.(Optional) Use the 2st slider to alter the speed (MAX = 1s)</pre>
+    <pre>3.Choose a sorting algorithm and enjoy the ride!</pre>
+    <pre>4.Repeat from step 1. and have fun 🙂</pre>
     </div>
 )}
